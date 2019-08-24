ac3d95a9cddff369c175e53f23a4b964d03f8eec
Beth Ashton <Mbeth.ashton@gmail.com>
2019-08-24 11:20:45 -0700

    undefined issue

--- planet/pangea.js
+++ planet/pangea.js
@@ -26,13 +26,51 @@ function rand(upperLimit, x){
   return (Math.floor(Math.random() * upperLimit) + lowerLimit)
 }
 
+function ifBlack(map, r, c){
+  //look at 4 directions
+
+  let sides = []
+
+  if (map[r - 1][c][0] != undefined){
+    sides.push(map[r - 1][c][0])
+  }
+  if (map[r][c -1][0] != undefined){
+    sides.push(map[r][c -1][0])
+  }
+  if (map[r][c + 1][0] != undefined){
+    sides.push(map[r][c + 1][0])
+  }
+  if (map[r + 1][c][0] != undefined){
+    sides.push(map[r + 1][c][0])
+  }
+
+  let select = rand(sides.length)
+
+  return sides[select]
+}
+
 function genPangea(width, height){
   let map = newMap(width, height)
 
-  map[90][180][0] = 1
-  console.log(map[90][180][0])
+  let startRow = 90
+  let startCol = 180
 
-  
+  map[startRow][startCol][0] = 1
+  console.log(map[startRow][startCol][0])
+
+  //let's go in triangle down from pixel
+  let nD = 1
+  for(rD = startRow + nD; rD < height; rD++){
+    map[rD][startCol][0] = ifBlack(map, rD, startCol);
+
+    for(i = 0; i < nD; i++){
+      //color in on each side
+      map[rD][startCol + i][0] = 1
+      map[rD][startCol - i][0] = 1
+    }
+
+    nD += 1;
+  }
 
   return map
 }
